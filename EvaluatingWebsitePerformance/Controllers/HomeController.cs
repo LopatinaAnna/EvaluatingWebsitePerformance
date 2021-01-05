168edfb00958e742a9c867d37f195f6e8fafa675
Anna Lopatina <an.lopatina050@gmail.com>
2021-01-05 18:55:29 +0200

    Add MeasureResponseTime action

--- EvaluatingWebsitePerformance/Controllers/HomeController.cs
+++ EvaluatingWebsitePerformance/Controllers/HomeController.cs
@@ -1,12 +1,54 @@
-﻿using System.Web.Mvc;
+﻿using System.Diagnostics;
+using System.Net;
+using System.Web.Mvc;
+using System.Web.Helpers;
 
 namespace EvaluatingWebsitePerformance.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult MeasureResponseTime(string url)
+        {
+            WebRequest request = WebRequest.Create(url);
+
+            Stopwatch timer = new Stopwatch();
+
+            double time;
+
+            timer.Start();
+            request.GetResponse();
+            timer.Stop();
+
+            time = timer.Elapsed.TotalSeconds;
+            ViewData["url"] = url;
+            return MeasureResponseTime(time);
+        }
+
+        [HttpGet]
+        public ActionResult MeasureResponseTime(double? time)
+        {
+            return View(System.Math.Round(time.GetValueOrDefault(), 3));
+        }
+
+        //public ActionResult CreateChart()
+        //{
+        //    var chart = new Chart(width: 500, height: 200)
+        //          .AddTitle("График посещений")
+        //          .AddSeries(
+        //                 name: "Моя программа",
+        //                 legend: "Моя программа",
+        //                 chartType: "Line",
+        //                 xValue: new[] { "Peter", "Andrew", "Julie", "Mary", "Dave" },
+        //                 yValues: new[] { "2", "6", "4", "5", "3" });
+
+        //    return File(chart.ToWebImage().GetBytes(), "image/jpeg");
+        //}
     }
 }
---
+++ EvaluatingWebsitePerformance/Scripts/get-chart.js
@@ -0,0 +1,55 @@
+Highcharts.chart('container', {
+    chart: {
+        type: 'column'
+    },
+    title: {
+        text: 'Evaluating website.com performance'
+    },
+    xAxis: {
+        categories: [
+            '1',
+            '2',
+            '3',
+            '4',
+            '5'
+        ],
+        crosshair: true
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Evaluating (seconds)'
+        }
+    },
+    tooltip: {
+        headerFormat: '<span style="font-size:10px">{point.key} attempt</span><table>',
+        pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+            '<td style="padding:0"><b>{point.y:.1f} s</b></td></tr>',
+        footerFormat: '</table>',
+        shared: true,
+        useHTML: true
+    },
+    plotOptions: {
+        column: {
+            pointPadding: 0.2,
+            borderWidth: 0
+        }
+    },
+    series: [{
+        name: 'website.com',
+        data: [0.423, 0.301, 0.435, 0.421, 0.378]
+
+    }, {
+        name: 'website.com/news',
+        data: [0.123, 0.201, 0.135, 0.221, 0.178]
+
+    }, {
+        name: 'website.com/about',
+        data: [0.423, 0.401, 0.335, 0.621, 0.678]
+
+    }, {
+        name: 'website.com/contacts',
+        data: [0.223, 0.323, 0.345, 0.521, 0.478]
+
+    }]
+});
